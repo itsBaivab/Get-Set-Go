a611ca445d03cab46e51e8c68436d54c6bdcb48f
itsBaivab <mukhopadhyaybaivab77@gmail.com>
2024-06-15 19:33:02 +0530

    chore: Refactor createMovie function and add updateMovie function

--- go-movies-curd/main.go
+++ go-movies-curd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -52,19 +53,35 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-
 //create movies function
 
-func createMovie(w http.ResponseWriter, r *http.Request){
+func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder((r.Body)).Decode(&movie)
 	movies = append(movies, movie)
-	movie.ID =strconv.Itoa(rand.Intn(100000000))
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
 	movies = append(movies, movie)
 
 }
 
+// Update movie functiion
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
+
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -80,5 +97,5 @@ func main() {
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Server is running on port 8000\n")
-	Log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
